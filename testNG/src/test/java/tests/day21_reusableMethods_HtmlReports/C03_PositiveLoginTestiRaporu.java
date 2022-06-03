5272b2658279e2488a9be03e79b252d1055174f1
Gokhan Agar <gokhanagar1@gmail.com>
2022-06-03 11:47:49 +0300

    V11

--- testNG/src/test/java/tests/day21_reusableMethods_HtmlReports/C03_PositiveLoginTestiRaporu.java
+++ testNG/src/test/java/tests/day21_reusableMethods_HtmlReports/C03_PositiveLoginTestiRaporu.java
@@ -18,18 +18,23 @@ public class C03_PositiveLoginTestiRaporu extends TestBaseRapor {
         //  https://www.bluerentalcars.com/ adresine git
         Driver.getDriver().get(ConfigReader.getProperty("brcUrl"));
         extentTest.info("Brc anasayfaya gidildi");
+        
         //      login butonuna bas
         brcPage.ilkLoginButonu.click();
         extentTest.info("login butonuna tiklandi");
+
         //test data user email: customer@bluerentalcars.com ,
         brcPage.emailTextBox.sendKeys(ConfigReader.getProperty("brcValidEmail"));
         extentTest.info("Gecerli email yazdirildi");
+
         //test data password : 12345
         brcPage.passwordTextBox.sendKeys(ConfigReader.getProperty("brcValidPassword"));
         extentTest.info("Gecerli sifre yazildi");
+
         //login butonuna tiklayin
         brcPage.ikinciLoginButonu.click();
         extentTest.info("Login butonuna tiklandi");
+
         //Degerleri girildiginde sayfaya basarili sekilde girilebildigini test et
         Assert.assertTrue(brcPage.kullaniciProfilIsmi.isDisplayed());
         extentTest.info("Kullanici basarili bir sekilde giris yapti");
