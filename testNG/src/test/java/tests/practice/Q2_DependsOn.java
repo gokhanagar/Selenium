e6a2d3eabfd8d009a3d9bf5c1182a9b7677d850a
Gokhan Agar <gokhanagar1@gmail.com>
2022-06-06 13:07:42 +0300

    V15

--- testNG/src/test/java/tests/practice/Q2_DependsOn.java
+++ testNG/src/test/java/tests/practice/Q2_DependsOn.java
@@ -9,10 +9,10 @@ public class Q2_DependsOn extends TestBase {
        /*
     birbirine bagimli testler olusturun..
     .beforClass olusturup setUp ayarlarini yapin..
-      birbirine bagimli testler olusturarak;
-          ilk once facebook a gidin
-          sonra facebook a bagimli olarak google a gidin,
-          daha sonra google a bagimli olarak amazon a gidin
+    birbirine bagimli testler olusturarak;
+    ilk once facebook a gidin
+    sonra facebook a bagimli olarak google a gidin,
+    daha sonra google a bagimli olarak amazon a gidin
     driver i kapatin
 
  */
