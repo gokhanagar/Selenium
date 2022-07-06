91e0822bc3261962e2ef7f3cc89db8039797ddb2
Gokhan Agar <gokhanagar1@gmail.com>
2022-07-06 11:05:04 +0300

    V28

---
+++ testNG/src/test/java/tests/seleniumgrid/SeleniumGrid.java
@@ -0,0 +1,42 @@
+package tests.seleniumgrid;
+
+import org.testng.annotations.Test;
+
+public class SeleniumGrid {
+
+    @Test
+    public void test1(){
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
