eea722601406bfc2c366ff515f6f5e6d0d4ee720
Gokhan Agar <gokhanagar1@gmail.com>
2022-08-25 18:19:26 +0300

    V67

---
+++ jUnit/src/test/java/engJunit/Day04_RadioButton.java
@@ -0,0 +1,74 @@
+package engJunit;
+
+import com.github.javafaker.Faker;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+
+public class Day04_RadioButton {
+    WebDriver driver;
+    Faker faker = new Faker();
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com/");
+    }
+
+    @Test
+    public void radio(){
+//        Create a class : RadioButton
+//        Complete the following task
+//        Go to https://www.facebook.com/
+//
+//        TEST CASE:
+//        Click on Create new account
+        driver.findElement(By.linkText("Create new account")).click();
+//        Enter first name
+        driver.findElement(By.name("firstname")).sendKeys(faker.name().firstName());//fake first name
+//        Enter last name
+        driver.findElement(By.name("lastname")).sendKeys(faker.name().lastName());//fake last name
+//        Enter mobile number or email
+        driver.findElement(By.name("reg_email__")).sendKeys(faker.phoneNumber().cellPhone());//fake phone
+//        Enter new password
+        driver.findElement(By.name("reg_passwd__")).sendKeys(faker.internet().password());//fake password
+//        Enter birthday(LATER)
+//        Jan / 10 / 2000
+//        1. Locate the dropdown element
+        WebElement monthElement = driver.findElement(By.xpath("//select[@id='month']"));
+//        2. Crereate select object
+        Select selectMonth = new Select(monthElement);
+//        3. Select Jan
+        selectMonth.selectByVisibleText("Jan");
+
+//        DAY
+        WebElement dayElement = driver.findElement(By.xpath("//select[@id='day']"));
+        Select selectDay = new Select(dayElement);
+        selectDay.selectByVisibleText("10");
+
+//        YEAR
+        WebElement yearElement = driver.findElement(By.xpath("//select[@id='year']"));
+        Select selectYear = new Select(yearElement);
+        selectYear.selectByVisibleText("2000");
+
+
+//        Enter gender
+        driver.findElement(By.xpath("(//input[@type='radio'])[2]")).click();
+//        Click Sign Up
+        driver.findElement(By.name("websubmit")).click();
+    }
+//    @After
+//    public void tearDown(){
+//        driver.quit();
+//    }
+}
---
+++ jUnit/src/test/java/engJunit/Day04_xpath_css.java
@@ -0,0 +1,49 @@
+package engJunit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day04_xpath_css {
+    WebDriver driver;
+    @Before
+    public void setUp(){
+//        Prerequisites
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+//        user goes to http://a.testaddressbook.com/sign_in
+        driver.get("http://a.testaddressbook.com/sign_in");
+    }
+    @Test
+    public void xpath_css(){
+        /*Locating username, password, and button using xpath*/
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys("testtechproed@gmail.com");
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("Test1234!");
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+    }
+
+    @Test
+    public void css(){
+        /*1. Locationg username, password, and button usign cssSelector
+        There is no // 2. There is no @ symbol */
+        driver.findElement(By.cssSelector("input[type='email']")).sendKeys("testtechproed@gmail.com");
+//        Alternative cssLocator for email : input#session_email  OR   #session_email
+//        Alternative cssLocator for email : input.form-control   OR   .form-control
+//        Note: # = id    and     . = class
+        driver.findElement(By.cssSelector("input[type='password']")).sendKeys("Test1234!");
+        driver.findElement(By.cssSelector("input[type='submit']")).click();
+    }
+//2:05 pm
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+
+}
