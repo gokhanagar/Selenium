6828fed1a847a9fd99aeaa8cfb1f06f619491a0f
Gokhan Agar <gokhanagar1@gmail.com>
2022-09-06 20:28:57 +0300

    V90

---
+++ jUnit/src/test/java/engJunit/Day08_MultipleWindowUtil.java
@@ -0,0 +1,24 @@
+package engJunit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+public class Day08_MultipleWindowUtil extends TestBase {
+
+    @Test
+    public void multipleWindowTest(){
+        driver.get("https://the-internet.herokuapp.com/windows");
+        System.out.println("Old Window : "+driver.getCurrentUrl());
+        driver.findElement(By.linkText("Click Here")).click();
+
+//        driver is currently on window 1. use switchToTargetWindow to switch tot eh other window
+
+//        Pass the title of the window that you want to switch to
+        switchToTargetWindow("New Window");//Switch to the window whose TITLE = New Window
+        System.out.println("New Window : "+ driver.getCurrentUrl());
+
+    }
+
+
+}
