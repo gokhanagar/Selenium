129038b2f761dc93106eaa16d806b2db14cbc793
Gokhan Agar <gokhanagar1@gmail.com>
2022-10-01 23:02:37 +0300

    V135

--- jUnit/src/test/java/engJunit/Day02_VerifyURLTest.java
+++ jUnit/src/test/java/engJunit/Day02_VerifyURLTest.java
@@ -26,30 +26,30 @@ public class Day02_VerifyURLTest {
             System.out.println("ACTUAL : " + actualURL);
             System.out.println("EXPECTED : " + expectedURL);
         }
-        /*
-        *
-FAILED
-ACTUAL : https://www.google.com/
-EXPECTED : www.google.com
-* What do you do when a test case fails?
-* 1. Rerun your test once more time
-* 2. Do manual testing to make sure
-* 3. Contact with Developer
-* 4. Contact with BA
-*   -Check if this is a document issue(spelling,...)
-*   -If that is a documentation issue, then let BA fix the docs
-*   -And correct your test case
-* 5. If that is actual bug???
-*   -Raise a bug ticket in JIRA
-*   -Assign is to the dev
-* 6. After dev fix the issue and deploy the code in test environment
-*   -Retest manually, then run your automation script
-*   -And close the ticket if all good!
-*
-* NOTE: each company has a different culture!
-*
-* When you found a bug, do you fix the bug your self?
-*   - Nope. I document and report.
-        * */
+             /*
+
+            FAILED
+            ACTUAL : https://www.google.com/
+            EXPECTED : www.google.com
+            * What do you do when a test case fails?
+            * 1. Rerun your test once more time
+            * 2. Do manual testing to make sure
+            * 3. Contact with Developer
+            * 4. Contact with BA
+            *   -Check if this is a document issue(spelling,...)
+            *   -If that is a documentation issue, then let BA fix the docs
+            *   -And correct your test case
+            * 5. If that is actual bug???
+            *   -Raise a bug ticket in JIRA
+            *   -Assign is to the dev
+            * 6. After dev fix the issue and deploy the code in test environment
+            *   -Retest manually, then run your automation script
+            *   -And close the ticket if all good!
+            *
+            * NOTE: each company has a different culture!
+            *
+            * When you found a bug, do you fix the bug your self?
+            *   - Nope. I document and report.
+            */
     }
 }
