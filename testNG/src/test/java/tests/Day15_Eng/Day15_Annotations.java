69d26159f6ee2f525c32dff0a6feffcbcf04268a
Gokhan Agar <gokhanagar1@gmail.com>
2022-10-04 07:53:49 +0300

    V140

--- testNG/src/test/java/tests/Day15_Eng/Day15_Annotations.java
+++ testNG/src/test/java/tests/Day15_Eng/Day15_Annotations.java
@@ -9,31 +9,38 @@ import org.testng.annotations.Test;
 public class Day15_Annotations {
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         System.out.println("Before Method");
     }
+
     @AfterMethod
-    public void tearDown(){
+    public void tearDown() {
         System.out.println("After Method");
     }
+
     @Test(priority = 2)
-    public void ztest1(){
+    public void ztest1() {
         System.out.println("Test 1");
     }
+
     @Test(priority = 1)
-    public void test2(){
+    public void test2() {
         System.out.println("Test 2");
     }
+
     @Test(enabled = false)
-    public void test3(){
+    public void test3() {
         System.out.println("Test 3");
     }
-    @Test @Ignore
-    public void test4(){
+
+    @Test
+    @Ignore
+    public void test4() {
         System.out.println("Test 4");
     }
+
     @Test(priority = -3)
-    public void atest5(){
+    public void atest5() {
         System.out.println("Test 5");
     }
 
@@ -48,22 +55,23 @@ public class Day15_Annotations {
 
     //@Test(priority = 0)
     @Test
-    public void test6(){
+    public void test6() {
         System.out.println("Test 6");
     }
+
     //@Test(priority = 0)
     @Test
-    public void test7(){
+    public void test7() {
         System.out.println("Test 7");
     }
 
     /*
-    * @Test : creates test case/method
-    * @BeforeMethod: Same as @Before in JUNIT. Runs before each method. Runs after each @TEST annotation
-    * @AfterMethod : Same as @After in JUNIT. Runs after each method. Runs after each @Test annotation
-    * @Ignore : To skip a test case
-    * @Test(enabled = false) : to disable a test case. Similar to Ignore
-    * @Test(priority = 1) : priority is used for running tests in specific order.
-    * By default, testNG runs in alphabetical order
+     * @Test : creates test case/method
+     * @BeforeMethod: Same as @Before in JUNIT. Runs before each method. Runs after each @TEST annotation
+     * @AfterMethod : Same as @After in JUNIT. Runs after each method. Runs after each @Test annotation
+     * @Ignore : To skip a test case
+     * @Test(enabled = false) : to disable a test case. Similar to Ignore
+     * @Test(priority = 1) : priority is used for running tests in specific order.
+     * By default, testNG runs in alphabetical order
      */
 }
