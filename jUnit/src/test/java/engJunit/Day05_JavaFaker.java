64c3cc610f926bc8ccf177e09b59939b17556928
Gokhan Agar <gokhanagar1@gmail.com>
2022-10-11 07:23:52 +0300

    V151

--- jUnit/src/test/java/engJunit/Day05_JavaFaker.java
+++ jUnit/src/test/java/engJunit/Day05_JavaFaker.java
@@ -4,28 +4,28 @@ import com.github.javafaker.Faker;
 import org.junit.Test;
 
 public class Day05_JavaFaker {
-//    fake data = mock data : data that is not real
+    //    fake data = mock data : data that is not real
 //    fake ssn, fake name, fake address
 //    we can use fake data to test
 //    we can use java faker class to generate fake data
-/*
-* Where do you get your data?
-* 1. BA=business analyst
-* 2. Test Lead
-* 3. Manual Test- We have a manual tester that is in the project for a long time. he/she provides test data
-* 4. Tech Lead = Team Tech = Dev Lead
-* */
+    /*
+     * Where do you get your data?
+     * 1. BA=business analyst
+     * 2. Test Lead
+     * 3. Manual Test- We have a manual tester that is in the project for a long time. he/she provides test data
+     * 4. Tech Lead = Team Tech = Dev Lead
+     * */
     @Test
-    public void fakerTest(){
+    public void fakerTest() {
 //        1. get Faker dependency
 //        2. Create a Faker object
-        Faker faker  = new Faker();
+        Faker faker = new Faker();
 //        3. generate fake data
         String fName = faker.name().firstName();
         System.out.println(fName);
         String lName = faker.name().lastName();
         System.out.println(lName);
-        String fullName=faker.name().fullName();
+        String fullName = faker.name().fullName();
         System.out.println(fullName);
         String username = faker.name().username();
         System.out.println(username);
@@ -41,7 +41,7 @@ public class Day05_JavaFaker {
         System.out.println(email);
         String randomZipCode = faker.number().digits(5);
         System.out.println(randomZipCode);
-        String fakeEmail = faker.name().firstName() + faker.number().digits(3)+faker.internet().emailAddress();
+        String fakeEmail = faker.name().firstName() + faker.number().digits(3) + faker.internet().emailAddress();
         System.out.println(fakeEmail);
     }
 
