6944fb590acc19fb6ecc47d8c07fe0def190ad33
Gokhan Agar <gokhanagar1@gmail.com>
2022-11-03 23:17:28 +0300

    183

--- testNG/src/test/java/tests/practice/Q11.java
+++ testNG/src/test/java/tests/practice/Q11.java
@@ -13,20 +13,21 @@ import java.time.Duration;
 import java.util.List;
 
 public class Q11 {
-         /*
-     ...Exercise...
-    // http://the-internet.herokuapp.com/add_remove_elements/
-    // click on the "Add Element" button 100 times
-    // write a function that takes a number, and clicks the "Delete" button
-    // given number of times, and then validates that given number of
-    // buttons was deleted
+    /*
+...Exercise...
+// http://the-internet.herokuapp.com/add_remove_elements/
+// click on the "Add Element" button 100 times
+// write a function that takes a number, and clicks the "Delete" button
+// given number of times, and then validates that given number of
+// buttons was deleted
 
-    1.method : createButtons()
-    2.deleteButtonsAndValidate()
- */
+1.method : createButtons()
+2.deleteButtonsAndValidate()
+*/
     WebDriver driver;
+
     @Before
-    public void setUp(){
+    public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -34,37 +35,37 @@ public class Q11 {
     }
 
     @Test
-    public void test(){
+    public void test() {
         driver.get("http://the-internet.herokuapp.com/add_remove_elements/");
-        createButtons(driver,100);
-        deleteButtonsAndValidate(driver,20);
+        createButtons(driver, 100);
+        deleteButtonsAndValidate(driver, 20);
     }
 
 
     public void createButtons(WebDriver driver, int number) {
         WebElement addButton = driver.findElement(By.xpath("//button[@onclick='addElement()']"));
-        for(int i = 1; i<= number; i++){
+        for (int i = 1; i <= number; i++) {
             addButton.click();
         }
     }
 
     public void deleteButtonsAndValidate(WebDriver driver, int numOfDelete) {
-      List<WebElement> elementBefore = driver.findElements(By.xpath("//button[@onclick='deleteElement()']"));
-      int sizeBeforeDelete = elementBefore.size();
+        List<WebElement> elementBefore = driver.findElements(By.xpath("//button[@onclick='deleteElement()']"));
+        int sizeBeforeDelete = elementBefore.size();
 
-      int counter = 0;
-      for(WebElement w : elementBefore){
-          counter++;
-          if(counter > numOfDelete){
-              break;
-          }
-          w.click();
-      }
+        int counter = 0;
+        for (WebElement w : elementBefore) {
+            counter++;
+            if (counter > numOfDelete) {
+                break;
+            }
+            w.click();
+        }
 
-      List<WebElement> elementAfter = driver.findElements(By.xpath("//button[@onclick='deleteElement()']"));
-      int sizeAfterDelete = elementAfter.size();
+        List<WebElement> elementAfter = driver.findElements(By.xpath("//button[@onclick='deleteElement()']"));
+        int sizeAfterDelete = elementAfter.size();
 
-       Assert.assertEquals(sizeBeforeDelete-numOfDelete,sizeAfterDelete);
+        Assert.assertEquals(sizeBeforeDelete - numOfDelete, sizeAfterDelete);
     }
 
 
