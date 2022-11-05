d5ee275cbba5325dececdb1bb521d9285343f713
Gokhan Agar <gokhanagar1@gmail.com>
2022-11-05 23:26:22 +0300

    186

--- testNG/src/test/java/tests/practice/Q12.java
+++ testNG/src/test/java/tests/practice/Q12.java
@@ -26,29 +26,29 @@ public class Q12 extends TestBase {
         driver.switchTo().frame(iframe);
 
         // click on the second emoji
-       WebElement secondEmoji = driver.findElement(By.xpath("(//span[@class='mdl-tabs__ripple-container mdl-js-ripple-effect'])[2]"));
-       secondEmoji.click();
+        WebElement secondEmoji = driver.findElement(By.xpath("(//span[@class='mdl-tabs__ripple-container mdl-js-ripple-effect'])[2]"));
+        secondEmoji.click();
 
         // click all the second emoji elements
-       List<WebElement> emojees = driver.findElements(By.xpath("//div[@id='nature']/div/img"));
+        List<WebElement> emojees = driver.findElements(By.xpath("//div[@id='nature']/div/img"));
 
 //       for (WebElement w : emojees){
 //           w.click();
 //       }
 
 //        by using lambda
-        emojees.stream().forEach(t-> t.click());
+        emojees.stream().forEach(t -> t.click());
         //go back default content
         driver.switchTo().defaultContent();
 
         // fill the form,(Fill the form with the texts you want)
-      List<WebElement> textBoxes =  driver.findElements(By.xpath("//input[@class='mdl-textfield__input']"));//11
+        List<WebElement> textBoxes = driver.findElements(By.xpath("//input[@class='mdl-textfield__input']"));//11
 
-      //This -iframe -example- looks- very- funny- does- not -it- ? - !
-      List<String> words = new ArrayList<>(Arrays.asList("This","iframe","example","looks","very","funny","does","not","it","?","!"));//11
+        //This -iframe -example- looks- very- funny- does- not -it- ? - !
+        List<String> words = new ArrayList<>(Arrays.asList("This", "iframe", "example", "looks", "very", "funny", "does", "not", "it", "?", "!"));//11
 //                                                         1        2      3         4       5      6       7     8     9   10   11
 
-        for(int i = 0; i<textBoxes.size(); i++){
+        for (int i = 0; i < textBoxes.size(); i++) {
             textBoxes.get(i).sendKeys(words.get(i));
         }
 
