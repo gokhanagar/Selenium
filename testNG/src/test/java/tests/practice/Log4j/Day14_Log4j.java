6b05e7bc0e7084497412f32c2c4e3be701bccd70
Gokhan Agar <gokhanagar1@gmail.com>
2022-11-17 21:32:48 +0300

    V189

---
+++ testNG/src/test/java/tests/practice/Log4j/Day14_Log4j.java
@@ -0,0 +1,29 @@
+package tests.practice.Log4j;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+
+public class Day14_Log4j {
+
+    private static Logger logger = LogManager.getLogger(Day14_Log4j.class.getName());
+
+    @Test
+    public void log4JDemo() {
+
+        logger.info("Info Log");
+        logger.debug("Debug Log");
+        logger.error("Error Log");
+        logger.fatal("Fatal Log");
+        logger.warn("Warn Log");
+
+        //1. Change the LEVEL to trace to see ALL LOGS
+        //2. Log the information on a file
+        //3. Do all there for the tests package ONLY
+
+
+    }
+
+
+}
