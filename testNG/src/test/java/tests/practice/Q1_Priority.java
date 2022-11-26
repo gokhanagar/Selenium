b19030b79e34b3605a8c44ae397cd98cbab14363
Gokhan Agar <gokhanagar1@gmail.com>
2022-11-26 22:32:28 +0300

    V193

--- testNG/src/test/java/tests/practice/Q1_Priority.java
+++ testNG/src/test/java/tests/practice/Q1_Priority.java
@@ -13,30 +13,30 @@ public class Q1_Priority {
       enabled = false  methodu : Testi gormezden gelmek icin @Test in yanina    '(enabled = false)' fonksiyonunu kullaniriz.
      */
 
-    @Test(priority =3000)
-    public void b (){
+    @Test(priority = 3000)
+    public void b() {
         System.out.println("b");
     }
 
-    @Test(priority=2001, enabled = false)
-    public void a (){
+    @Test(priority = 2001, enabled = false)
+    public void a() {
         System.out.println("a");
     }
 
-    @Test(priority=2000)
-    public void c (){
+    @Test(priority = 2000)
+    public void c() {
         System.out.println("c");
     }
 
-/* ilk etapta cikti:
-    c
-    a
-    b
- enabled = false kullandigimda o test gormezden gelinir
+    /* ilk etapta cikti:
+        c
+        a
+        b
+     enabled = false kullandigimda o test gormezden gelinir
 
-    c
-    b
- */
+        c
+        b
+     */
     @Test
     public void test01() {
         System.out.println("test01");
@@ -48,17 +48,17 @@ public class Q1_Priority {
     }
 
 
-    @Test( enabled = false)
+    @Test(enabled = false)
     public void test03() {
         System.out.println("test03");
     }
 
-    @Test ( enabled = false)
+    @Test(enabled = false)
     public void test04() {
         System.out.println("test04");
     }
 
-    @Test ( enabled = false)
+    @Test(enabled = false)
     public void test05() {
         System.out.println("test05");
     }
@@ -76,4 +76,4 @@ public class Q1_Priority {
     b
      */
 
-    }
+}
