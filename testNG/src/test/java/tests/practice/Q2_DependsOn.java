02ae4d2e8050f72f043cea3273542dace5b32e74
Gokhan Agar <gokhanagar1@gmail.com>
2022-12-02 00:33:32 +0300

    V195

--- testNG/src/test/java/tests/practice/Q2_DependsOn.java
+++ testNG/src/test/java/tests/practice/Q2_DependsOn.java
@@ -18,17 +18,17 @@ public class Q2_DependsOn extends TestBase {
  */
 
     @Test
-    public void facebookTest(){
+    public void facebookTest() {
         driver.get("http://www.facebook.com");
     }
 
-    @Test(dependsOnMethods="facebookTest")
-    public void googleTest(){
+    @Test(dependsOnMethods = "facebookTest")
+    public void googleTest() {
         driver.get("http://www.google.com");
     }
 
-    @Test(dependsOnMethods="googleTest")
-    public void amazonTest(){
+    @Test(dependsOnMethods = "googleTest")
+    public void amazonTest() {
         driver.get("http://www.amazon.com");
     }
 
