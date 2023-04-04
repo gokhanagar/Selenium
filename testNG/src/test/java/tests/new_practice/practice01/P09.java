03a638b291aa02261503d9945e5d969eeca4e6ec
gokhan agar <gokhanagar1@gmail.com>
2023-04-04 22:11:18 +0300

    V236

---
+++ testNG/src/test/java/tests/new_practice/practice01/P09.java
@@ -0,0 +1,13 @@
+package tests.new_practice.practice01;
+
+public class P09 {
+    /*
+    - Amazon.com'a gidelim.
+    - Kategorilerin hepsini konsola yazdiralim
+    - DropDown üzerinde Books secelim.(All yazan yerde)
+    - sonra sectigimiz option'i yazdiralim
+    - Arama kutusuna coding yazalım ve arama yapalim.
+    - Sonuc sayisini ekrana yazdiralim.
+    - Sonucların coding i icerdigini assert edelim
+     */
+}
